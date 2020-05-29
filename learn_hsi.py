fa9c6c1311131472ea7a5742bf8f6e3b40002f40
Jakub Siwiec <jakubsiwiec.nwm@gmail.com>
2020-05-29 16:41:02 +0200

    Change methodology order - 1. LBP, 2. patch

--- learn_hsi.py
+++ learn_hsi.py
@@ -18,28 +18,47 @@ print("Maximum value: {}".format(np.argmax(data)))
 
 print("Index of maximum value: {}".format(np.unravel_index(np.argmax(data), data.shape)))
 
-'''
-Crop patches
-'''
-
-data_patches_hsi = get_data.get_patches(data, 9)
-print(data_patches_hsi[1])
-print("Shape of patches array: {}\n".format(data_patches_hsi.shape))
-
-print(data_patches_hsi[0,0,0])
-
-get_data.grey_to_lbp(data_patches_hsi[0,0,0])
 
 # print(get_data._hs_to_grey(data_patches_hsi[8,5,0]))
 
+# apply lbp on image
+lbp_image = get_data.grey_to_lbp(data)
+
+#crop patches
+data_patches_lbp = get_data.get_patches(data, 3)
+
+#compare greyscale image with lbp map
 plt.figure(1)
-plt.imshow(get_data.hs_to_grey(data_patches_hsi[90,80,0]), cmap= 'gray')
+plt.imshow(get_data.hs_to_grey(data), cmap= 'gray')
+plt.title("Grayscale image")
+
+plt.figure(2)
+plt.imshow(get_data.hs_to_grey(lbp_image), cmap= 'gray')
+plt.title("LBP image")
+
+# present some patches of lbp data
+plt.figure(3)
+co_x = 0
+co_y = 0
+plt.imshow(get_data.hs_to_grey(data_patches_lbp[co_x, co_y, 0]), cmap='gray')
+plt.title("LBP sample patch of coordinates {}, {}".format(co_x, co_y))
+
+
+
+
+
+
+
+
+
+
+
+
 plt.show()
 
 
 
-'''
-Layer to LBP
-'''
+
+
 
 
